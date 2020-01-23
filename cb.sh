1eccdda8d91d9607aa2e355e7112208dbf16cdaf
Will Long <will.long043@gmail.com>
2020-01-23 05:39:51 -0500

    Installs ChiBio OS as a systemd service. Relative paths have been replaced with absolute paths and variables for install location. setup.sh now has a guard for root only, all uses of sudo have been removed. apt also upgrades.

--- cb.sh
+++ cb.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Uncomment the following line to run ChiBio in the background
 # screen -dmS ChiBio bash -c "gunicorn -b 192.168.7.2:5000 app:application"
 # Then, comment out the next line
--- setup.sh
+++ setup.sh
@@ -1,19 +1,32 @@
-cd ../../etc/ssh/
+#!/bin/bash
+
+installdir="/root/chibio/"
+repodir=$(dirname "$(readlink -f "$0")")
+
+if [[ $(id -u) -ne 0 ]]; then
+	echo "This script must be run as root"
+	exit 1
+fi
+
+cd /etc/ssh/
 echo "PermitRootLogin yes" >> sshd_config
 echo -e "root\nroot" | passwd root
 sed -i 's@-w /var/lib/cloud9@-w /root/chibio@' /lib/systemd/system/cloud9.service
 sed -i 's@1000@root@' /lib/systemd/system/cloud9.service
-cd ..
+cd /etc/
 echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" >> resolv.conf
-sudo /sbin/route add default gw 192.168.7.1
-sudo apt-get update
-mkdir ~/chibio
-cd ~/../home/debian
-cp cb.sh ~/chibio
-cp app.py ~/chibio
-cp static -r ~/chibio
-cp templates -r ~/chibio
-sudo apt --assume-yes install python-pip
+/sbin/route add default gw 192.168.7.1
+systemctl restart systemd-timesyncd
+apt update
+apt --assume-yes upgrade
+mkdir -p $installdir
+cd $repodir
+cp cb.sh $installdir
+cp app.py $installdir
+cp static -r $installdir
+cp templates -r $installdir
+cp chibio.service /etc/systemd/system/chibio.service
+apt --assume-yes install python-pip
 pip install Gunicorn
 pip install flask
 pip install serial
@@ -22,12 +35,15 @@ pip install --user --upgrade setuptools
 pip install simplejson
 pip install smbus2
 pip install numpy
-cd ~/../tmp
+cd /tmp/
 pip download Adafruit_BBIO
 tar xvzf Adafruit_BBIO-1.1.1.tar.gz
 cd Adafruit_BBIO-1.1.1/
 sed -i "s/'-Werror', //g" setup.py
 python setup.py install
-cd ~/../root/chibio
+cd $installdir
 chmod +x cb.sh
+systemctl daemon-reload
+systemctl enable chibio
+systemctl start chibio
 reboot now
